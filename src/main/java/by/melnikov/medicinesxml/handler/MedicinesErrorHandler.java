9b05bff23df46865fd7b14feff75fc0125109ce3
Slava Melnikov <simplejkee@gmail.com>
2023-11-19 16:56:17 +0300

    add validator and error handler

---
+++ src/main/java/by/melnikov/medicinesxml/handler/MedicinesErrorHandler.java
@@ -0,0 +1,29 @@
+package by.melnikov.medicinesxml.handler;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class MedicinesErrorHandler implements ErrorHandler {
+    private static final Logger logger = LogManager.getLogger();
+    @Override
+    public void warning(SAXParseException e) throws SAXException {
+        logger.warn(getLineColumnNumber(e) + " - " + e.getMessage());
+    }
+
+    @Override
+    public void error(SAXParseException e) throws SAXException {
+        logger.error(getLineColumnNumber(e) + " - " + e.getMessage());
+    }
+
+    @Override
+    public void fatalError(SAXParseException e) throws SAXException {
+        logger.fatal(getLineColumnNumber(e) + " - " + e.getMessage());
+    }
+
+    private String getLineColumnNumber(SAXParseException e) {
+        return e.getLineNumber() + " : " + e.getColumnNumber();
+    }
+}
